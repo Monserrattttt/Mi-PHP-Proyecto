e11d61369ac2b7b31b85a6427a50d5831e112369
Monserrattttt <gonzalezrubi377@gmail.com>
2025-07-09 01:45:58 -0600

    Delete db.php

--- db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect("sqlXXX.infinityfree.com", "epiz_XXXXXX", "yourpassword", "epiz_XXXXXX_jerseypro");
-if (!$conn) {
-    die("Conexión fallida: " . mysqli_connect_error());
-}
-?>
